e6915e53215c8fca94ff5270ceecaacde430cf1c
ralleyne531 <ralleyne531@gmail.com>
2023-12-22 01:01:02 -0500

    12.22: Created the functionality of creating, storing, and displaying random messages. Deployed in its first version

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
+const keypress = require('keypress');
+
+
 console.log("Hello World!");
+
+setTimeout(() =>{
+    console.log( "Welcome to mixed messages!");
+},3000)
+
+setInterval(() =>{
+    console.log("Like this website? Visit forums for updates!")
+},60000);
+
+//two-dimensional array, where number keys each have a "text" value
+let messages = ["Message one", "Message two", "Message three,"];
+
+//random selection through range of 'messages'
+let randomizer = Math.floor(Math.random() * messages.length);
+
+//redirect input to single key press
+keypress(process.stdin);
+
+setTimeout(() =>{
+    console.log("press m to receive a random message");
+    //Create an event for user pressing the correct key, "m"
+    process.stdin.on('keypress', function (ch, key) {
+        if (key && key.name === 'm') {
+            //logic for when the user presses 'm'
+            console.log(messages[randomizer],randomizer);
+            process.exit(); // Optional: exit the program after processing the keypress
+        }
+    });
+}, 5000);
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
